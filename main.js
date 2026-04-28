ab51f832995d2889f49b7426a909e95a8e0f3be4
Muffin5671 <213143890+Muffin5671@users.noreply.github.com>
2026-04-28 11:30:34 -0400

    remade to add newlines

---
+++ main.js
@@ -0,0 +1,24 @@
+class PolymodMeta {
+  constructor(modName, modDesc, modHome, modContributors, modApi, modVer) {
+    return `{
+      title: ${modName},
+      description: ${modDesc},
+      homepage: ${modHome},
+      contributors: ${modContributors},
+      api_version: ${modApi},
+      mod_version: ${modVer},
+      license: 'Apache-2.0'
+    }`;
+  }
+}
+
+$('#download')[0].addEventListener('click', () => {
+  saveAs(new File([new PolymodMeta(
+    $('#modName')[0].value,
+    $('#modDesc')[0].value,
+    $('#modHome')[0].value,
+    [{name: 'someone', role: 'something'}],
+    $('#modApi')[0].value,
+    $('#modVer')[0].value
+  )], '_polymod_meta.json'));
+})
