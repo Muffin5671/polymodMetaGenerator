4216123428b5c0c64696c31de36bdbd0af30db87
Muffin5671 <213143890+Muffin5671@users.noreply.github.com>
2026-06-01 13:42:35 -0400

    update

--- js/script.js
+++ js/script.js
@@ -1,3 +1,15 @@
+$("#add").addEventListener("click", () => {
+  let cont = document.createElement("div");
+  cont.className = "cont";
+  let cName = document.createElement("input");
+  let cRole = document.createElement("input");
+  let cURL = document.createElement("input");
+  cont.appendChild(cName);
+  cont.appendChild(cRole);
+  cont.appendChild(cURL);
+  $(".box")[0].appendChild(cont);
+})
+
 function generatePolymodMeta() {
   let meta = {};
 
@@ -5,13 +17,26 @@ function generatePolymodMeta() {
   meta.description = $("#desc")[0].value;
   if (!($("#home")[0].value == "")) meta.homepage = $("#home")[0].value;
   for (let i = 0; i < $(".cont").length; i++) {
-    meta.contributors.push();
+    let contributor = {};
+
+    contributor.name = $(".cont")[i].children[0].value;
+    contributor.role = $(".cont")[i].children[1].value;
+    if (!($(".cont")[i].children[2].value == "")) contributor.url = $(".cont")[i].children[2].value;
+
+    meta.contributors.push(contributor);
   }
-  meta.contributors;
+  meta.api_version = $("#apiVer")[0].value;
+  meta.mod_version = $("#apiVer")[0].value;
+  if (!($("#license")[0].value == "")) meta.license = $("#license")[0].value;
 
   return JSON.stringify(meta, undefined, 2);
 }
 
-$("#download").addEventListener("click", () => {
-  
+$("#download")[0].addEventListener("click", () => {
+  let file = new File(
+    [generatePolymodMeta()],
+    "_polymod_meta.json",
+  );
+
+  saveAs(file);
 });
