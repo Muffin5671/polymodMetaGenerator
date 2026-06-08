807213b346bef3f56aac35e6344a13f3bcf0f8a6
Muffin5671 <213143890+Muffin5671@users.noreply.github.com>
2026-06-08 12:53:09 -0400

    json upload

--- js/script.js
+++ js/script.js
@@ -4,15 +4,15 @@ $("#add")[0].addEventListener("click", () => {
   let cNameLabel = document.createElement("p");
   cNameLabel.innerText = "Name:";
   let cName = document.createElement("input");
-  cName.placeholder = "Username";
+  cName.setAttribute("placeholder", "Username");
   let cRoleLabel = document.createElement("p");
   cRoleLabel.innerText = "Role:";
   let cRole = document.createElement("input");
-  cRole.placeholder = "Creator / Musician";
+  cRole..setAttribute("placeholder", "Creator / Musician");
   let cURLLabel = document.createElement("p");
   cURLLabel.innerText = "URL (leave blank to not include):";
   let cURL = document.createElement("input");
-  cURL.placeholder = "https://example.com/";
+  cURL.setAttribute("placeholder", "https://example.com/");
   cont.appendChild(cNameLabel);
   cont.appendChild(cName);
   cont.appendChild(cRoleLabel);
@@ -62,4 +62,46 @@ $("#download")[0].addEventListener("click", () => {
   );
 
   saveAs(file);
+});
+
+$("#loadFile")[0].addEventListener("click", () => {
+  let fileInput = document.createElement("input");
+  fileInput.setAttribute("type", "file");
+  fileInput.setAttribute("accept", "application/json");
+  fileInput.click();
+
+  onfocus = () => {
+    if (fileInput.files[0] == undefined) onfocus = null
+    else {
+      fileInput.files[0].text()
+      .then(content => {
+        let meta = JSON.parse(content);
+
+        $("#title")[0].value = meta.title;
+        $("#desc")[0].value = meta.description;
+        if (!(meta.homepage == undefined)) $("#home")[0].value = meta.homepage
+        else $("#home")[0].value = "";
+        if (!($(".cont").length == 0)) {
+          do {
+            $(".cont")[0].remove();
+           } while (!($(".cont").length == 0))
+        }
+        let i = 0;
+        meta.contributors.forEach(cont => {
+          $("#add")[0].click();
+          $(".cont")[i].children[1].value = cont.name;
+          $(".cont")[i].children[3].value = cont.role;
+          if (!(cont.url == undefined)) $(".cont")[i].children[5].value = cont.url
+          else $(".cont")[i].children[5].value = "";
+          i++;
+        })
+        $("#apiVer")[0].value = meta.api_version;
+        $("#modVer")[0].value = meta.mod_version;
+        if (!(meta.license == undefined)) $("#license")[0].value = meta.license
+        else $("#license")[0].value = "";
+        $("#whitespace")[0].checked = /\n/.test(content);
+      });
+      onfocus = null;
+    }
+  }
 });
